fc3dfa001f8423bc10ea3823da9fd30e0d91eef1
user <example@gmail.com>
2023-10-16 11:24:18 +0000

    squashed commits

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+//test git
+//	test git 2
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
